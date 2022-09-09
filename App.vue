230891cd943bdaec6ca9ed1915e11366f7c4cc46
zys8119 <zys112425>
2022-09-09 20:14:54 +0800

    [add] 初始化项目

---
+++ App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="App">
+            asdasd
+    </div>
+</template>
+
+<script setup lang="ts">
+onMounted(()=>{
+    console.log(111)
+})
+</script>
+
+<style scoped lang="less">
+.App {
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig} from "vite"
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins:[
+        vue(),
+        AutoImport({
+            include: [
+                /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+                /\.vue$/, /\.vue\?vue/, // .vue
+                /\.md$/, // .md
+            ],
+            imports:[
+                "vue"
+            ],
+            dts:true
+        }),
+        Components()
+    ]
+})
