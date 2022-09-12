3783f5b2d4556cb8b38e608ee14e283394eca8d6
zys8119 <770959294@qq.com>
2022-09-12 11:02:00 +0800

    [add] 扫码页面

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pass-code
 通行码
+
+>[访问地址](https://zys8119.github.io/pass-code/)
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import {resolve} from 'path'
 
 export default defineConfig({
     base:"",
+    build:{
+      outDir:"./docs"
+    },
     plugins:[
         vue(),
         AutoImport({
